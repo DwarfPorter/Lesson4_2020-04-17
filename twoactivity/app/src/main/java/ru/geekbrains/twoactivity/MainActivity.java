2864f808d0047b535d09f308a3c669afebc25db1
DwarfPorter <mrvlmor@gmail.com>
2020-04-17 20:46:35 +0300

    Передача данных во вторую активити

--- twoactivity/app/src/main/java/ru/geekbrains/twoactivity/MainActivity.java
+++ twoactivity/app/src/main/java/ru/geekbrains/twoactivity/MainActivity.java
@@ -5,19 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Constants {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText text = findViewById(R.id.editText);
         Button startAct = findViewById(R.id.button);
         startAct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+                intent.putExtra(TEXT, text.getText().toString());
                 startActivity(intent);
             }
         });
--- twoactivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
+++ twoactivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
@@ -4,19 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.Objects;
+
+public class SecondActivity extends AppCompatActivity implements Constants {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        String text = getIntent().getStringExtra(TEXT);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(text);
+
         Button retToMain = findViewById(R.id.button2);
         retToMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                onBackPressed();
+                finish();
             }
         });
     }
