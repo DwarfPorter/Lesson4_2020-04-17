c0fedc5d70f3d94796ef1c4fd2bd2164daba7d1c
DwarfPorter <mrvlmor@gmail.com>
2020-04-17 20:33:37 +0300

    возврат по кнопке из 2-й активити

--- twoactivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
+++ twoactivity/app/src/main/java/ru/geekbrains/twoactivity/SecondActivity.java
@@ -2,6 +2,8 @@ package ru.geekbrains.twoactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button retToMain = findViewById(R.id.button2);
+        retToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
